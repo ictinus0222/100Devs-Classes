9f091a54b0aa90b9255ccd7decbca5fa3d954116
Akhil Sharma <sharmakhil1704@gmail.com>
2025-06-25 22:31:12 +0530

    Updated arrays-in-it-to-win-it

--- class19-materials/arrays-in-it-to-win-it/js/main.js
+++ class19-materials/arrays-in-it-to-win-it/js/main.js
@@ -5,6 +5,6 @@ let theOneNotWatched = movies[0];
 //Get the length of the original array and store it in a new variable
 let len = movies.length;
 //Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
-let n = movies.length;
+let n = (movies.length - 1);
 
 let lastVar = movies[n];
