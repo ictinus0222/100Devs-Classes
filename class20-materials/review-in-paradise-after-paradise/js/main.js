902ec10e11bdeb7ce47487ec9e70b45869fa7ea0
Akhil Sharma <sharmakhil1704@gmail.com>
2025-06-28 09:56:17 +0530

    Completed review in paradise

--- class20-materials/review-in-paradise-after-paradise/js/main.js
+++ class20-materials/review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,10 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+function Hi(arr){
+    if(arr[0] < arr[arr.length]){
+        alert('Hi');
+    } else if(arr[0] > arr[arr.length]) {
+        alert('Bye');
+    } else {
+        alert('We close in an hour')
+    }
+}
--- hitlist-appwrite-main/src/shhh.js
+++ hitlist-appwrite-main/src/shhh.js
@@ -1,4 +1,4 @@
 // shhh.js
-export const PROJECT_ID = "";
+export const PROJECT_ID = "685e3aa6003d9512ba8f";
 export const DATABASE_ID = "685e3cdf00241edf98fa"
 export const COLLECTION_ID = "685e3cfb00241765338f"
