3d5ecc58b9f741f1bf7b665fc20a368d0b5893e6
Akhil Sharma <sharmakhil1704@gmail.com>
2025-06-28 17:55:26 +0530

    Completed review-winter-games

--- class20-materials/review-winter-games/js/main.js
+++ class20-materials/review-winter-games/js/main.js
@@ -1,1 +1,13 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+function makeNewEvenArray(oldArr) {
+    let newArr = [];
+    
+    oldArr.forEach(n => {
+        if(n % 2 === 0){
+            newArr.push(n);
+        }
+    });
+    alert(newArr);
+}
+
+makeNewEvenArray([0,1,2,3,4,5,6,7,8,9,10])
