8ded7a8686f9318aa35dc241c438bde010b29bc4
Akhil Sharma <sharmakhil1704@gmail.com>
2025-06-30 18:29:26 +0530

    completed objects-zeo

--- class21-materials/objects-zeo/js/main.js
+++ class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,21 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {
+    color: "Blue",
+    brand: "casio",
+    time: "1200 hours"
+}
+
+stopwatch.brand = "Nike"
+stopwatch.time = "1300 Hours"
+stopwatch.color = "Black"
+stopwatch.size = "45mm"
+
+stopwatch.sayBrand = function () {
+    console.log(stopwatch.brand);
+}
+stopwatch.startTimer = function() {
+    console.log("STARTINGGGGG!");
+}
+stopwatch.stopTimer = function(){
+    console.log("Stopped.");
+}
