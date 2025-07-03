322c6b907995336110d7707023b84ec1d845621b
Akhil Sharma <sharmakhil1704@gmail.com>
2025-07-03 17:05:37 +0530

    completed space-review

--- class22-materials/space-review/js/main.js
+++ class22-materials/space-review/js/main.js
@@ -1,10 +1,15 @@
 //Arrays
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
+let arrOfNumbers = [2,34,56,76,434,65];
+let sum = arrOfNumbers.reduce((acc,c) => acc + c, 0)
 
 //Create a function that takes in an array of numbers
+function squaringNumbers() {
 //Return a new array of numbers that is every original number squared
-
+const newArray = originalArray.map((number) => {number ** 2});
+console.log(newArray);
+}
 //Create a function that takes string
 //Print the reverse of that string to the console
 
