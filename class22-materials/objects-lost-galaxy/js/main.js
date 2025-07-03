8a231ae24bb3f7b297d08a5b9ddeb76da49b728d
Akhil Sharma <sharmakhil1704@gmail.com>
2025-07-03 17:40:36 +0530

    completed objects-lost-galaxy

--- class22-materials/objects-lost-galaxy/js/main.js
+++ class22-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,21 @@
 //Create a mouse object that has four properties and three methods
+let mouse = {}
+
+    mouse.buttons =  7;
+    mouse.color = 'Black';
+    mouse.brand = 'RedGear';
+    mouse.spec = 'Gaming';
+
+    mouse.rightClick() 
+    {
+        console.log('RIGHT CLICK')
+    }
+    mouse.leftClick()
+    { 
+        console.log('LEFT CLICK')
+    } 
+    mouse.scroll()
+    {
+        console.log('Scroll');
+    }
+
