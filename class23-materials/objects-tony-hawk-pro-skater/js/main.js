6e253b45bfeabae9c3f5b91e9d83677321776c88
Akhil Sharma <sharmakhil1704@gmail.com>
2025-07-04 23:17:01 +0530

    solved objects-tony-hawk-pro-skater

--- class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,20 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+function TonyHawkProSkater (name, ability, level, superMove, taunt) {
+    this.name = name;
+    this.ability = ability;
+    this.level = level;
+    this.superMove = superMove;
+    this.taunt = taunt;
+
+    this.jump = function() {
+        console.log(`SMOOTHHHHH OPERATORRR`);
+    }
+
+    this.tauntGesture = function() {
+        console.log(`${this.taunt}`);
+    }
+
+    this.specialMoveGo = function () {
+        console.log(`Let's ${this.superMove}`);
+    }
+}
