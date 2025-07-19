d995e6e43e6f6d6e30fa85181075055091fed47a
ictinus0222 <sharmakhil1704@gmail.com>
2025-07-19 11:31:06 +0000

    Completed objects-lost-galaxy

---
+++ class26-materials/objects-lost-galaxy/js/main.js
@@ -0,0 +1,18 @@
+//Create a pizza object that has four properties and three methods
+
+let pizza = {}
+
+pizza.brand = 'Dominoes';
+pizza.crust = 'Stuffed Crust';
+pizza.size = 'large';
+pizza.toppings = ['Garlic', 'onion', 'mushroom', 'paneer'];
+
+pizza.estimatedDeliveryTime = function(){
+    console.log('Calculating....')
+}
+pizza.burnMouth = function(){
+    console.log('AAAAHHHHHHHHhhhhh...')
+}
+pizza.frisbee = function(){
+    console.log('YEEEEEEEEEEEEE!')
+}
