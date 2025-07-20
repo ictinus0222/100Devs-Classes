12d5febbb2a45e62fb84a51b2cb36ad347186922
ictinus0222 <sharmakhil1704@gmail.com>
2025-07-20 11:38:08 +0000

    Completed objects-dog-photos

--- class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,22 @@
 //Create a constructor with 4 properties and 3 methods
+function PizzaMachine (pizzaCrust, pizzaSauce, pizzaToppings, pizzaBrand) {
+
+    this.crust = pizzaCrust;
+    this.sauce = pizzaSauce;
+    this.toppings = pizzaToppings;
+    this.brand = pizzaBrand;
+
+    pizzaBrand.makeDough = function (){
+
+    }
+
+    pizzaBrand.assemble = function(){
+
+    }
+
+    pizzaBrand.heat = function() {
+
+    }
+}
+
+let pizza = new PizzaMachine('Stuffed', 'Hot garlic', ['paneer', 'mushrooms', 'olives'], 'Dominoes');
